9d417d5d9bed3c8ade27d77d8cb2425a68f849a3
vpratnicki <vanessa@pratnicki.net>
2022-09-01 16:07:50 -0400

    Added current date and started implementing time variable

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,41 @@
 
 // Display today's day and date
 var todayDate = moment().format('dddd, MMM Do YYYY');
+var todayTime = moment().format("hh:mm A");
+
 $("#currentDay").html(todayDate);
+$("#currentTime").html(todayTime);
+
+  // convert to moment object at 5pm
+  var time = moment(date, "L").set("hour", 17);
+
+  // apply new clsss if event is in the past
+  // apply new class if event is in the present
+  // apply new class if event is in the future
+  if (moment().isBefore(time)){
+    $(".time-block").addClass("future");
+  } 
+  else if (moment().isSame(time)) {
+        $(".time-block").addClass("present");
+  }
+  else (moment().isAfter(time)) {
+    $(".time-block").addClass("past");
+  }
+
+
+
+
+
 
 
 $(".saveBtn").click(function () {
     //get values for time and event text 
-    let time = $(this).parent().parent().attr('id');
+    let hour = $(this).parent().parent().attr('id');
     console.log($(this).parent().parent().attr('id'));
     let text = $(this).parent().siblings('.time-block').children('.description').val();
     console.log($(this).parent().siblings('.time-block').children('.description').val());
 
-    localStorage.setItem(time, text)
+    localStorage.setItem(hour, text)
 });
     
 // Get item from local storage if any
