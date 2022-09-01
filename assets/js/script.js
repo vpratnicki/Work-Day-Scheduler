7b19d77f71ef1d80a069919a686cf8bebab3ef0d
vpratnicki <vanessa@pratnicki.net>
2022-09-01 13:38:49 -0400

    attemping to fix local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,43 +3,35 @@
 var todayDate = moment().format('dddd, MMM Do YYYY');
 $("#currentDay").html(todayDate);
 
-$(document).ready(function () {
-  // save button was clicked
-  $(".saveBtn").on("click", function () {
-    // event.preventDefault();
-    // get values of the event description
-    var eventText = $(this).siblings("description").val();
-    // get value of the time
-    var eventTime = $(this).parent().attr("id");
-
-    //save text in local storage
-    localStorage.setItem(eventTime, eventText);
-  })
-});
+var events = {};
 
 
-// Get item from local storage if any
-$("#900 .description").val(localStorage.getItem("900"));
-$("#1000 .description").val(localStorage.getItem("1000"));
-$("#1100 .description").val(localStorage.getItem("1100"));
-$("#1200 .description").val(localStorage.getItem("1200"));
-$("#1300 .description").val(localStorage.getItem("1300"));
-$("#1400 .description").val(localStorage.getItem("1400"));
-$("#1500 .description").val(localStorage.getItem("1500"));
-$("#1600 .description").val(localStorage.getItem("1600"));
-$("#1700 .description").val(localStorage.getItem("1700"));
+// save button in modal was clicked
+$("#900 .btn-save").click(function () {
+    // get form values
+    var eventText = $("#event-text").val();
+    var eventTime = $("#time").val();
 
+    console.log(eventText, eventTime)
 
-// var events = {};
+      // save in tasks array
+      events.push({
+        text: eventText,
+        time: eventTime
+      });
+  
+      saveEvents();
+    }
+  );
 
-// var saveEvents = function () {
-//     localStorage.setItem("events", JSON.stringify(events));
-//   };
+var saveEvents = function () {
+localStorage.setItem("events", JSON.stringify(events));
+};
 
-// var loadEvents = function () {
-//     events = JSON.parse(localStorage.getItem("events"));
+var loadEvents = function () {
+events = JSON.parse(localStorage.getItem("events"));
 
-//       // if nothing in localStorage, create a new object to track all event time arrays
+// // if nothing in localStorage, create a new object to track all event time arrays
 //   if (!events) {
 //     events = {
 //       nine: [],
@@ -56,3 +48,14 @@ $("#1700 .description").val(localStorage.getItem("1700"));
 //     });
 //   });
 // };
+
+// // Get item from local storage if any
+// $("#900 .description").val(localStorage.getItem("900"));
+// $("#1000 .description").val(localStorage.getItem("1000"));
+// $("#1100 .description").val(localStorage.getItem("1100"));
+// $("#1200 .description").val(localStorage.getItem("1200"));
+// $("#1300 .description").val(localStorage.getItem("1300"));
+// $("#1400 .description").val(localStorage.getItem("1400"));
+// $("#1500 .description").val(localStorage.getItem("1500"));
+// $("#1600 .description").val(localStorage.getItem("1600"));
+// $("#1700 .description").val(localStorage.getItem("1700"));
