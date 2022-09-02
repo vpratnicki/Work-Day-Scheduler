d68426feb2c88dbcef989fa4ede51d40b5db5fcc
vpratnicki <vanessa@pratnicki.net>
2022-09-02 19:44:56 -0400

    converted strings into numbers to compare times

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,70 +1,83 @@
-
 // Display today's day and date
-var todayDate = moment().format('dddd, MMM Do YYYY');
+var todayDate = moment().format("dddd, MMM Do YYYY");
 var todayTime = moment().format("hh:mm A");
 
 $("#currentDay").html(todayDate);
 $("#currentTime").html(todayTime);
 
+function compareTime() {
+  // get current number of hours.
+  // loop through the time rows
+  $(".time-block").each(function () {
+    const theElement = $(this); // that has timeblock
+    const theElementsId = theElement.attr("id"); // turn: timeBlock-9
+    const theElementTime = theElementsId.replace("timeBlock-", ""); // into: 9
+    //console.log(theElementTime);
+    const timeValueInRows = parseInt(theElementTime);
 
-  // compare current time with 'all hours' but need to get
+    // convert the current time string into number
+    const todayTimeHour = todayTime.slice(0, 2); // changes time string into: 10:00 AM in 10
+    console.log(todayTimeHour);
+    const todayHourAsNumber = parseInt(todayTimeHour);
 
+    //convert into military time
+    const timeValueInRowsAsMilitary = function () {
+        if (timeValueInRows === 1) {
+            // add 12 here
+            timeValueInRows += 12;
+        } else if (timeValueInRows === 2) {
+            // add 12 here
+            timeValueInRows += 12;
+        } else if (timeValueInRows === 3) {
+            // add 12 here
+            timeValueInRows += 12;
+        } else if (timeValueInRows === 4) {
+           // add 12 here
+           timeValueInRows += 12;
+        } else if (timeValueInRows === 5) {
+            // add 12 here
+            timeValueInRows += 12;
+        } else {
+            console.log(timeValueInRows)
+        }
+    };
 
-
-  function compareTime() {
-    // //get current number of hours.
-    // var timeNow = moment().hour();
-
-    // loop through the time rows
-    $(".time-block").each(function () {
-        var timeValueInRows = parseInt($(this).attr("id").split("hour")[1]);
-        
+    console.log(timeValueInRows, todayHourAsNumber);
     // apply new clsss if event is in the past
     // apply new class if event is in the present
     // apply new class if event is in the future
-    if (timeValueInRows < todayTime) {
-       $(this).removeClass("future");
-       $(this).removeClass("present");
-       $(this).addClass("past");
-
-    } 
-    else if (timeValueInRows === todayTime) {
-        $(this).removeClass("past");
-        $(this).removeClass("future");
-        $(this).addClass("present");
-    } 
-    else {
-        $(this).removeClass("past");
-        $(this).removeClass("future");
-        $(this).addClass("future");
+    if (timeValueInRows < todayHourAsNumber) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else if (timeValueInRows === todayHourAsNumber) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("future");
     }
-})
-  }
-
-//   $.each(allHours, function (i) {
-//     var hourId = parseInt($(this).attr("id"));
-//     if (hourId === currentTime) {
-//       $(this).next().addClass("present");
-//     } else if (hourId < currentTime) {
-//       $(this).next().addClass("past");
-//     } else if (hourId > currentTime) {
-//       $(this).next().addClass("future");
-//     }
-//   });
-// }
-// compareTime();
-
+  });
+}
 
 $(".saveBtn").click(function () {
-    //get values for time and event text 
-    let hour = $(this).parent().parent().attr('id');
-    console.log($(this).parent().parent().attr('id'));
-    let text = $(this).parent().siblings('.time-block').children('.description').val();
-    console.log($(this).parent().siblings('.time-block').children('.description').val());
+  //get values for time and event text
+  let hour = $(this).parent().parent().attr("id");
+  console.log($(this).parent().parent().attr("id"));
+  let text = $(this)
+    .parent()
+    .siblings(".time-block")
+    .children(".description")
+    .val();
+  console.log(
+    $(this).parent().siblings(".time-block").children(".description").val()
+  );
 
-    localStorage.setItem(hour, text)
+  localStorage.setItem(hour, text);
 });
-    
+
 // Get item from local storage if any
 $("#900 .description").val(localStorage.getItem("900"));
 $("#1000 .description").val(localStorage.getItem("1000"));
